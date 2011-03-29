d53c2303985ee6275a7f130245f76420246b7e8d
Harrison Weingard <h4rr@u.washington.edu>
2011-03-29 00:06:28 -0700

    Removed /test/ handler from urls, moved it to sc2tourney.

--- urls.py
+++ urls.py
@@ -4,5 +4,4 @@ from sc2tournament.models import Player
 urlpatterns = patterns('sc2tournament.views',
     (r'^player/list/$', 'list_players'),
     (r'^player/search/$', 'search'),
-    (r'^player/test/$', 'test_search_page'),
 )

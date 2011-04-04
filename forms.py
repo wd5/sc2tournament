64fa356423f7760351bc56c4a68a119234f41a96
Harrison Weingard <h4rr@u.washington.edu>
2011-04-03 23:56:59 -0700

    Added simple form for Tournaments.

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
-from django.forms import ModelForm, Form
+from django import forms
 from sc2tournament.models import Player, Tournament, Team
 
-class PlayerForm(ModelForm):
+class PlayerForm(forms.ModelForm):
     """
     Simple ModelForm that only shows the data we would expect a user to be able
     to enter.
@@ -9,3 +9,16 @@ class PlayerForm(ModelForm):
     class Meta:
         model  = Player
         fields = ('name', 'character_code', 'region')
+
+class TournamentForm(forms.ModelForm):
+    """
+    Simple ModelForm to create new Tournaments
+    """
+    #eventually we will add optional passwords to tournaments.. but not now..
+    #password1 = forms.CharField(help_text="Password", max_length=20, widget=forms.PasswordInput)
+    #password2 = forms.CharField(help_text="Re-enter password", max_length=20, widget=forms.PasswordInput)
+
+
+    class Meta:
+        model = Tournament
+        fields = ('name', 'best_of')

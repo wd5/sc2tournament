7c6887aadf0acaa25263a07db7c62a42bcff1742
Spencer Thomas <srt4@uw.edu>
2011-05-15 15:59:39 -0700

    Fixing functionality of drag-n-drop

--- static/js/tourney_manage.js
+++ static/js/tourney_manage.js
@@ -1,24 +1,24 @@
 // Some ajaxy stuff for submitting the form
 $(document).ready(function() {
+     // Handy function to toggle between two side-by-side <ul> elements
+     // Accepts a ul element within a ul element, moves it over to the next one
+     $.fn.listToggle = function() {
+        
+     };
+    
 	// Accepts the <li> containing the team's <form> and accepts or rejects them
 	// Returns true if the ajax post succeeds
 	// Returns the error message otherwise
 	$.fn.ajaxPost = function() {
-	    var accept = "{% url sc2tournament.views.tournament_accept %}";
-	    var reject = "{% url sc2tournament.views.tournament_reject %}";
+         $(this).addClass("ajax-loading");
 	    var link = $(this).children('form');
 	    // Get the form data to submit
-	    var data = link.serialize();
+   	    var data = link.serialize();
 	    // Get the URL that the form submits it to
 	    var postUrl = link.attr('action');
-	    // Conditional stuff to consolidate accepted/rejected into one function
-	    var accepted = postUrl == accept;
-	    if(accepted) console.log("Called with an accept url");
-	    var newUrl = accepted ? reject : accept;
-	    var appendClass = accepted ? "#accepted_teams" : "#pending_teams";
 	    // Allow pass-by-reference for the success section of the ajax request
 	    function returnObj() {
-		this.value = -1;
+      		this.value = -1;
 	    }
 	    var returnString = new returnObj();
 	    $.ajax({
@@ -29,88 +29,28 @@ $(document).ready(function() {
               //pass the data         
               data: data,     
               //Do not cache the page
-              cache: false,
+              cache: false, 
               //success
               success: function(html) {
-		    // error checking
-		    if (html.indexOf('<html>') != -1) {
-			console.log($(html).text());
-		    } else {
-			$("#error").text("There was an error! " + response +  " (click to close) ");
-			$("#error").fadeIn();
-			$("#error").click(function () {
-			    $(this).fadeOut();
-			}); 
-		    }
+        		    // error checking
+        		    if (html.indexOf('<html>') != -1) {
+              		$('.ajax-loading').children('form').attr("action", $('.ajax-loading').parents('div').attr("data-post-url"));
+                    	$('.ajax-loading').removeClass("ajax-loading").removeClass("loading");
+                    	$('.ajax-loading').toggleClass("pending").toggleClass("accepted");
+        		    } else {
+          			$("#error").text("There was an error! " + html);
+          			$("#error").fadeIn();
+          			$("#error").click(function () {
+          			    $(this).fadeOut();
+          			}); 
+        		    }
 	      }
 	    });
 	    return returnString.value;
 	};
 	
-	$.fn.ajaxToggle = function() {
-	  var team = this;
-          console.log("Called with " + team);
-          var accept = "{% url sc2tournament.views.tournament_accept %}";
-          var reject = "{% url sc2tournament.views.tournament_reject %}";
-          // Get the parent - i.e., the form element, this function is called from <a>
-          var link = $(team).parent();
-          // Get the form data to submit
-          var data = link.serialize();
-          // Get the URL that the form submits it to
-          var postUrl = link.attr('action');
-          // Conditional stuff to consolidate accepted/rejected into one function
-          var accepted = postUrl == accept;
-          if(accepted) console.log("Called with an accept url");
-          var newUrl = accepted ? reject : accept;
-          var appendClass = accepted ? "#accepted_teams" : "#pending_teams";
-          console.log("Posting to " + postUrl);
-          $.ajax({
-              // AJAX request to the URL the form was going to submit to
-              url: postUrl, 
-              // Post ... 
-              type: "POST",
-              //pass the data         
-              data: data,     
-              //Do not cache the page
-              cache: false,
-              //success
-              success: function (html) {              
-                  // TODO: we can check the result here
-                  if ( html.indexOf('<html>') != -1 ) {
-                      // Move up to the <li> tag
-                      link = link.parent();
-                      // Fade it out, move it to accepted teams, fade in.              
-                      link.fadeOut(100, function() {      
-                        link.remove();
-			link.appendTo(appendClass + " ul:first ");
-                        link.fadeIn(100, function() {       
-                          $(this).children('form').attr('action', newUrl);
-                         // TODO: Methinks there's a more efficient way to do this
-			             $('.pending , .accepted').unbind('click');
-			             $('.pending , .accepted').click(function() { $(this).ajaxToggle() });
-                        });
-                      });
-                  } else { // Deal with an ajax error
-			$("#error").text("There was an error! " +html+ " (click to close) ");
-			$("#error").fadeIn();
-			$("#error").click(function () {
-				$(this).fadeOut();
-			});
-		   }
-		    
-              }
-          });
-	  return -1;
-        };
-	
-	// add the listeners
-        //$('.pending , .accepted').click(function() { $(this).ajaxToggle() });
-	
-	// from jquery ui
-	$( "#pending_list" ).sortable({
-		connectWith: "ul"
-	});
-	$("#accepted_list").sortable({
+    // from jquery ui - make the lists connected and sortable
+	$("#pending_list, #accepted_list").sortable({
 		connectWith: "ul"
 	});
 	
